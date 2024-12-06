451a88b2af68ea90da2e6fac8ccaf4fd69d637a8
Cezary Androsiuk PC <czandros@wp.pl>
2024-12-06 22:43:38 +0100

    added Event

---
+++ cpp/Event.h
@@ -0,0 +1,15 @@
+#ifndef EVENT_H
+#define EVENT_H
+
+#include <QObject>
+
+class Event : public QObject
+{
+    Q_OBJECT
+public:
+    explicit Event(QObject *parent = nullptr);
+
+signals:
+};
+
+#endif // EVENT_H
