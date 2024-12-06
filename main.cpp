18f635db78a41a67f922f520cba7619eecd6de07
Cezary Androsiuk PC <czandros@wp.pl>
2024-12-06 22:44:21 +0100

    main and CMakeLists

--- main.cpp
+++ main.cpp
@@ -3,13 +3,27 @@
 
 #include <QQmlContext>
 
+#include "cpp/DoubleStartProtection.h"
+#include "cpp/Login.h"
+#include "cpp/Backend.h"
+#include "cpp/support/LogQML.h"
+// #include "cpp/storages/Memory.h"
+
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    // Backend *backend = new Backend(&app);
-    // engine.rootContext()->setContextProperty("Backend", backend);
+    Backend *backend = new Backend(&app);
+    Login *login = new Login(&app);
+
+    LogQML *logQML = new LogQML(&app);
+
+    engine.rootContext()->setContextProperty("DoubleStartProtection", DoubleStartProtection::getInstance());
+    engine.rootContext()->setContextProperty("Backend", backend);
+    engine.rootContext()->setContextProperty("Login", login);
+
+    engine.rootContext()->setContextProperty("log", logQML);
 
     QObject::connect(
         &engine,
@@ -17,11 +31,11 @@ int main(int argc, char *argv[])
         &app,
         []() { QCoreApplication::exit(-1); },
         Qt::QueuedConnection);
-    // QObject::connect(
-    //     &engine,
-    //     &QQmlApplicationEngine::objectCreated,
-    //     Initializer::getInstance(),
-    //     &Initializer::initialize);
+    QObject::connect(
+        &engine,
+        &QQmlApplicationEngine::objectCreated,
+        DoubleStartProtection::getInstance(),
+        &DoubleStartProtection::verify);
     engine.loadFromModule("PetanqueTeam", "Main");
 
     return app.exec();
