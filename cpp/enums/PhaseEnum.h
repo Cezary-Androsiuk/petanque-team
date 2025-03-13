de7ae7b3b4693ace304ed092ecfb1f2cfa8ef4b0
Cezary-Androsiuk <czandros@wp.pl>
2025-03-13 14:34:10 +0100

    implemented EnumConvert for PhaseEnum

--- cpp/enums/PhaseEnum.h
+++ cpp/enums/PhaseEnum.h
@@ -1,9 +1,20 @@
 #ifndef PHASEENUM_H
 #define PHASEENUM_H
 
+#include <QString>
+
+#include "cpp/support/Log.h"
+
 enum PhaseEnum{
     First,
     Second,
 };
+
+
+namespace EnumConvert{
+QString PhaseToQString(PhaseEnum phase);
+PhaseEnum QStringToPhase(const QString &phase);
+}
+
 
 #endif // PHASEENUM_H
